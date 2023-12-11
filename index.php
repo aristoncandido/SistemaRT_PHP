e7f0a6426561fb78309418bb85dec66ddd3dd4e1
Ariston Cândido da Silva Filho <ariston.filho@coren-pe.com.br>
2023-12-11 15:11:54 -0300

    Sistema RT php

---
+++ index.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300&display=swap" rel="stylesheet">
+    <title>Sistema RT</title>
+</head>
+
+<body>
+
+    <div class="conteiner">
+
+
+        <header>
+
+            <img src="./IMGS/logo.png" alt="logo">
+
+            <h1>
+                REGISTRO DE RESPONSABILIDADE TÉCNICA
+            </h1>
+
+
+        </header>
+
+        <main>
+
+            <div class="cards">
+
+                <div class="card bg1">Concessão
+
+
+                    <button>
+                        ACESSAR
+                    </button>
+
+
+
+                </div>
+                <div class="card bg2">Renovação
+
+
+                    <button>
+                        ACESSAR
+                    </button>
+
+
+
+                </div>
+                <div class="card bg1">Cancelamento
+
+                    <button>
+                        ACESSAR
+                    </button>
+
+
+                </div>
+                <div class="card bg2">Consultar
+
+                    <button>
+                        ACESSAR
+                    </button>
+
+                </div>
+
+            </div>
+
+
+
+        </main>
+
+
+    </div>
+
+
+</body>
+
+</html>
